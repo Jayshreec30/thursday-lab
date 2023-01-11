0b144521380e885afc25d665c54d6f1a54bf0427
Jayshreec30 <117347292+Jayshreec30@users.noreply.github.com>
2023-01-11 14:56:54 +0530

    hh

---
+++ 5_january/CIMSThrusdayLab/AnudipFoundation.java
@@ -0,0 +1,14 @@
+package CIMSThrusdayLab;
+
+import CollectionFramework.AnudipFoundationMenu;
+
+public class AnudipFoundation {
+	
+	public static void main (String []args)
+	{
+		System.out.println("\t\t WELCOME TO ANUDIP FOUNDATION ");
+		
+		AnudipFoundationMenu.menu();
+	}
+
+}
